2d6ce09aaae64bf0184fea839e7d6710d3531a20
Farid Heka <faridhkrio926@gmail.com>
2024-11-16 09:41:41 +0700

    ganti template

--- handel/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once '../inc/conn.php';
-
-session_start();
-unset($_SESSION['user_id']);
-
-header("location:../index.php");
--- inc/success.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once 'inc/conn.php';
-if (isset($_SESSION['success'])) {
-    ?>
-        <div class="alert alert-success"><?php echo $_SESSION['success']; ?></div>
-    <?php
-}
-unset($_SESSION['success']);
-?>
